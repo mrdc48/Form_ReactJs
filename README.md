bf4e4b072d77c419bf5c062ecae58f6a0a28f8f5
mrdc48 <dhotrechetan1@gmail.com>
2021-06-26 13:09:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-8hcid6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-8hcid6)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <input type="text" placeholder="firstName" name="firstName" onChange={} />
+      <br/>
+      <input type="text" placeholder="email" name="Email" />
+      <br/>
+      <button>submit</button>
+      <p></p>
+      <p></p>
+    </div>
+  );
+}
